4d3a951570eadd075e363b26186084d75470ee9d
Zeke <www.zeke5431269742163.com>
2019-05-16 22:29:11 +0800

    组员第一次提交修改

--- test1/src/com/zeke/test1.java
+++ test1/src/com/zeke/test1.java
@@ -6,5 +6,6 @@ public class test1 {
         System.out.println("git测试2");
         System.out.println("git测试3");
         System.out.println("git测试4");
+        System.out.println("git测试5");
     }
 }
